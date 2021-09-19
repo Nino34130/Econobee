39de811151cc141a3622f9699f753dd4e90a079a
Nino34130 <67977026+Nino34130@users.noreply.github.com>
2021-09-19 17:47:53 +0200

    Debut Présentation
    Demarrage de la page d'accueil + Création de la nav-bar + Création de la partie présentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    },
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': '#EECF64',
+      'secondary': '#FAF9F9',
+      'danger': '#e3342f',
+    })
   },
   variants: {
     extend: {},
