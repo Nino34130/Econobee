2851a1e8df006d0082061644d575ee3c0108bc74
Nino34130 <67977026+Nino34130@users.noreply.github.com>
2021-10-15 17:13:57 +0200

    fin du footer
    rrajout d'image + text au footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontSize: {
-      sm: ['14px', '20px'],
-      base: ['16px', '24px'],
-      lg: ['20px', '28px'],
-      xl: ['24px', '32px'],
-    },
-    backgroundColor: theme => ({
-      ...theme('colors'),
-      'primary': '#EECF64',
-      'secondary': '#545454',
-      'danger': '#e3342f',
-    })
+      textColor: theme => theme('colors'),
+      textColor: {
+        'primary': '#3490dc',
+        'secondary': '#ffed4a',
+        'danger': '#e3342f',
+      }
   },
   variants: {
     extend: {},
